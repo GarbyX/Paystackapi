edccc3d61d272fa687277aa0102f65b5423b9f4c
Gabriel Madichie <amadichie@yahoo.com>
2023-02-05 01:24:53 +0100

    1st commit

---
+++ src/main/java/com/garby/fintech/paystackapi/PaystackapiApplication.java
@@ -0,0 +1,13 @@
+package com.garby.fintech.paystackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystackapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaystackapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garby/fintech/paystackapi/PaystackapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garby.fintech.paystackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystackapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
